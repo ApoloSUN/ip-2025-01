3affd5ee8d31303f3303db57925dcf57a993c3ff
IanCarlosLT <xxxiancarlosxxx@gmail.com>
2025-04-23 22:51:11 -0300

    Update q1.go

--- lista04/q1.go
+++ lista04/q1.go
@@ -1,1 +1,23 @@
+package main
 
+import "fmt"
+
+func main() {
+	var list[10]int
+	var i int 
+	var menores = 0 
+
+	for i = 0; i < 10; i = i + 1 {
+		fmt.Scanf("%d", &list[i])
+		if list[i] > 50 {
+			fmt.Println("Número:", list[i], "no índice", i)
+		} else {
+			menores = menores + 1
+		} 
+		
+	}
+	
+	if menores == 10 {
+		fmt.Println("Não possui nenhum número superior a 50")
+    }
+}
